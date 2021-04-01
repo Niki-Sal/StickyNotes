8c9d1eaf8737532193c289c3298fea95c6219418
niki-sal <ne.salehi@gmail.com>
2021-04-01 11:25:13 -0700

    Add database and books id routes

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('books/', views.books_index, name='books_index'),
+    path('books/<int:book_id>/', views.books_show, name='books_show')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
 
 # Create your views here.
 def index(request):
@@ -9,16 +10,10 @@ def about(request):
     return render(request, 'about.html')
 
 
-class Book:
-    def __init__(self, name, author):
-        self.name = name
-        self.author = author
-      
-
-books = [
-    Book('Lolo', 'lili'),
-    Book('koko', 'kiki'),
-    Book('momo', 'mimi')
-]
 def books_index(request):
+    books = Book.objects.all()
     return render(request, 'books/index.html', {'books': books})
+
+def books_show(request, book_id):
+    book = Book.objects.get(id=book_id)
+    return render(request, 'books/show.html', {'book': book})
