3d7e256abce03e3afac355844b5f88fff2495500
niki-sal <ne.salehi@gmail.com>
2021-04-01 14:31:41 -0700

    add new routes

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+# Import the User
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Book(models.Model):
@@ -7,6 +11,8 @@ class Book(models.Model):
     notetype = models.CharField(max_length=120)
     image = models.CharField(max_length=500)
     date = models.DateTimeField(auto_now_add =True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
